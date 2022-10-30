5b648189ac4d38e5677b56e22fd76a4044a2cd34
Hedrauta <H3draut3r@hotmail.de>
2022-10-30 05:50:01 +0100

    theoretically works
    but i can't use it in my simulator q.q

--- convert-midi.ps1
+++ convert-midi.ps1
@@ -4,149 +4,127 @@ $path = $PSScriptRoot
 $mf2t = "$path\mf2tXP.exe"
 $conv_midi = "$path\c.txt"
 $output = "$path\output.txt"
-$output2 = "$path\output2.txt"
-$script:work_array = @()
-$script:work_array2 = @()
-$Script:tempo = 1
 # Functions
 
 
 function cMtext($file) {
-    $firstend = $true
+    $script:work_array = @()
+    $tempo = 1
     foreach ($line in Get-Content -Path $file) {
         $split = $($line.Split(" "))
         if ($split[1] -eq "Tempo") {
-            $script:tempo = 1 / ($($split[2]) / 500000)
-        }
-        if ($($($split[0]) -contains "T") -and $firstend) {
-            $script:work_array2 = $work_array
-            $script:work_array = @()
-            $firstend = $false
+            $tempo = 1 / ($($split[2]) / 500000)
         }
         if ( ($split[0] -match '^[0-9]+$') -and ($split[1] -ccontains "On")) {
             $time = [int]$split[0]
             $note = [int]$split[3].TrimStart("n=")
             $vol = [int]$split[4].TrimStart("v=")
-            $script:work_array += New-Object -TypeName psobject -Property @{Timing = $time; Note = $note; Volume = $vol }
+            $state = if($split[1] -contains "On") {$true} else {$false}
+            $script:work_array += New-Object -TypeName psobject -Property @{State = $state; Timing = $time; Note = $note; Volume = $vol; Speed = $tempo; Port = 0 }
         }
     }
     $script:work_array = $script:work_array | Sort-Object -Property Timing
-    $script:work_array2 = $script:work_array2 | Sort-Object -Property Timing
 }
 function calcNoteTime() {
+    function MathSpeed($time, $speed) {
+        return [Math]::Floor($time / $speed)
+    }
     $script:time_array = @()
-    $script:time_array2 = @()
     foreach ($ent in $script:work_array) {
         if ($ent.Volume -gt 0) {
-            $filt = $($script:work_array | Where-Object { $_.Note -eq $ent.Note -and $_.Volume -eq 0 -and $_.Timing -gt $ent.Timing } )[0]
-            $script:time_array += New-Object -TypeName psobject -Property @{TStart = $ent.Timing; TDur = $($filt.timing - $ent.Timing - $ent.Volume); Note = $ent.Note; Volume = $ent.Volume }
+            $filt = $($script:work_array | Where-Object { $_.Note -eq $ent.Note -and $($_.Volume -eq 0 -or $_.State -eq $false) -and $_.Timing -gt $ent.Timing } )[0]
+            $script:time_array += New-Object -TypeName psobject -Property @{TStart = $(MathSpeed $ent.Timing $ent.speed); TDur = $(MathSpeed $($filt.timing - $ent.Timing) $ent.speed); Note = $ent.Note; Port = $ent.Port}
         }
     }
-    foreach ($ent in $script:work_array2) {
-        if ($ent.Volume -gt 0) {
-            $filt = $($script:work_array2 | Where-Object { $_.Note -eq $ent.Note -and $_.Volume -eq 0 -and $_.Timing -gt $ent.Timing })[0]
-            $script:time_array2 += New-Object -TypeName psobject -Property @{TStart = $ent.Timing; TDur = $($filt.timing - $ent.Timing); Note = $ent.Note; Volume = $ent.Volume }
-        }
-    }
-    $script:time_array2 = $time_array2 | Sort-Object -Property TStart
     $script:time_array = $time_array | Sort-Object -Property TStart
 }
 
-function Trksort() {
-    # splits "double" timed notes from the midi and send it to the right time_array
-    $firstindex = $false
-    $corrected_array = $script:time_array2 | ConvertTo-Json | ConvertFrom-Json
-    foreach($ent in $script:time_array2) {
+function Portsort() {
+    # Set note to correct port, if in another port for each "time-array"
+
+    $firstindex = $true
+    $CorrectedObject = @()
+    foreach ($entry in $time_array) {
         if ($firstindex) {
-            $ix = [Array]::indexOf($time_array2, $ent)
-            $pix = $time_array2[$ix -1]
-            if($ent.TStart -ge $pix.TStart -and $ent.TStart -le $($pix.TStart + $pix.TDur)) {
-                $corrected_array = $corrected_array | Where-Object {$_.TStart -ne $ent.TStart}
-                $script:time_array += New-Object -TypeName psobject -Property @{TStart = $ent.TStart; TDur = $ent.TDur; Note = $ent.Note; Volume = $ent.Volume }
+            $CorrectedObject += $entry
+            $firstindex = $false
+        }
+        elseif (!$firstindex) {
+            $pick = $null
+            $filter = $CorrectedObject | Where-Object {$entry.Tstart -ge $_.Tstart -and $entry.Tstart -le ($_.TStart + $_.TDur)}
+            if ($filter.length -gt 0) {
+                $pick = $filter[$filter.length -1]
+            }
+            if ($null -ne $pick) {
+                $CorrectedObject += New-Object -TypeName psobject -Property @{Note = $entry.Note; Speed = $entry.Speed; TStart = $entry.Tstart; TDur = $entry.TDur; Port = $($pick.Port + 1)}
+            }
+            else {
+                $CorrectedObject += $entry
             }
         }
-        else {$firstindex = $true}
-    }
-    $script:time_array2 = $corrected_array | ConvertTo-Json | ConvertFrom-Json
-    "-"
-    $firstindex = $false
-    $corrected_array = $script:time_array | ConvertTo-Json | ConvertFrom-Json
-    foreach($ent in $script:time_array) {
-        if ($firstindex) {
-            $ix = [Array]::indexOf($time_array, $ent)
-            $pix = $time_array[$ix -1]
-            if($ent.TStart -ge $pix.TStart -and $ent.TStart -le $($pix.TStart + $pix.TDur)) {
-                $corrected_array = $corrected_array | Where-Object {$_.TStart -ne $ent.TStart}
-                $script:time_array2 += New-Object -TypeName psobject -Property @{TStart = $ent.TStart; TDur = $ent.TDur; Note = $ent.Note; Volume = $ent.Volume }
-            }
-        }
-        else {$firstindex = $true}
     }
     
-    $script:time_array = $corrected_array | ConvertTo-Json | ConvertFrom-Json | Sort-Object -Property TStart
-    $script:time_array2 = $script:time_array2 | Sort-Object -Property TStart
+    $script:time_array = $CorrectedObject | ConvertTo-Json | ConvertFrom-Json | Sort-Object -Property TStart
 }
 
-function ToFile() {
+function MathAndWrite() {
     # prepare code for arduino 1
-    function prepare_lines($out) {
-        "//" | Out-File -FilePath $out -Append
-        "void setup()" | Out-File -FilePath $out -Append
-        "{" | Out-File -FilePath $out -Append
-        "  pinMode(0, OUTPUT);" | Out-File -FilePath $out -Append
-        "}" | Out-File -FilePath $out -Append
-        "//" | Out-File -FilePath $out -Append
-        "void loop()" | Out-File -FilePath $out -Append
-        "{" | Out-File -FilePath $out -Append
+    function prepare_lines() {
+        "// Code written by a Powershell-Script" | Out-File -FilePath $output -Append
+        "void setup()" | Out-File -FilePath $output -Append
+        "{" | Out-File -FilePath $output -Append
+        "  pinMode(0, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(1, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(2, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(3, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(4, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(5, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(6, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(7, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(8, OUTPUT);" | Out-File -FilePath $output -Append
+        "  pinMode(9, OUTPUT);" | Out-File -FilePath $output -Append
+        "}" | Out-File -FilePath $output -Append
+        "//" | Out-File -FilePath $output -Append
+        "void loop()" | Out-File -FilePath $output -Append
+        "{" | Out-File -FilePath $output -Append
     }
-    function ConvertTo-Ard() {
-        $array = $args[0] | ConvertFrom-Json
-        if($script:outp -eq 1) {
-            $out = $output
-        }
-        else {
-            $out = $output2
-        }
+    function ConvertTo-Ard($array) {
+        $script:end = $null
         foreach ($entry in $array) {
+            # TODO: MATH!!!!!!!
             $freq = [System.Math]::Round(440 * [System.Math]::Pow(2, (($entry.Note - 69) / 12)))
-            $ini = [Array]::indexOf($array, $entry)
-            $delay = [Math]::round($entry.TDur / $script:tempo)
-            if($ini -lt $array.length -1) {
-                $delay = [Math]::round($($array[$ini + 1].TStart - $entry.TStart) / $script:tempo)
-            }
-            if ($ini -eq 0) {
-                if ($entry.TStart -gt 0) {
-                    "delay($([Math]::round($entry.TStart / $script:tempo)));" | Out-file -FilePath $out -Append # Initial Delay, if first note is off-time
+            $i = [Array]::indexOf($array, $entry)
+            if ($entry.Port -eq 0) {
+                if ($null -ne $script:end) {
+                    $pix = $time_array[$i - 1]
+                    "delay($($($entry.TStart - $script:end) + $($script:end - $pix.TStart)));" | Out-File -FilePath $output -Append;
                 }
+                $script:end = $entry.TStart + $entry.TDur
+                "tone($($entry.Port), $($freq), $($entry.TDur));" | Out-File -FilePath $output -Append
             }
-            "tone(0, $freq, $([Math]::round($entry.TDur / $script:tempo)));" | Out-File -FilePath $out -Append
-            "delay($($delay));" | Out-File -FilePath $out -Append
+            elseif ($entry.Port -ne 0) {
+                $pix = $time_array[$i - 1]
+                if ($($entry.TStart - $pix.TStart) -gt 0) {
+                    "delay($($entry.TStart - $pix.TStart));" | Out-File -FilePath $output -Append
+                }
+                if ($script:end -lt $($entry.TStart + $entry.TDur)) {
+                    $script:end = $entry.TStart + $entry.TDur
+                }
+                "tone($($entry.Port), $($freq), $($entry.TDur));" | Out-File -FilePath $output -Append
+            }
         }
     }
+    
     Remove-Item $output -ErrorAction Ignore -Force
     "//Ard 1" | Out-File -FilePath $output -Append
-    prepare_lines($output)
-    $argctj = $time_array | ConvertTo-Json
-    $script:outp = 1 
-    ConvertTo-Ard($argctj)
-    # Todo: "Sync" ending-Delay
+    prepare_lines
+    ConvertTo-Ard $time_array
     "delay(1000);" | Out-File -FilePath $output -Append
     "}" | Out-File -FilePath $output -Append
 
-    ## code for arduino 2
-    Remove-Item $output2 -ErrorAction Ignore -Force
-    "//Ard 2" | Out-File -FilePath $output2 -Append
-    prepare_lines($output2)
-    $argctj = $time_array2 | ConvertTo-Json
-    $script:outp = 2 
-    ConvertTo-Ard($argctj)
-    # Todo: "Sync" ending-Delay
-    "delay(1000);" | Out-File -FilePath $output2 -Append
-    "}" | Out-File -FilePath $output2 -Append
 }
 
-    
-
+# TODO: do some serious math
 
 ""
 "Preparing midi..." #change filename to another midi-file
@@ -154,9 +132,9 @@ Start-Process -FilePath $mf2t -WorkingDirectory $path -Wait -ArgumentList "$path
 "prepare text"
 cMtext($conv_midi)
 "Timings"
-calcNoteTime($work_array, $work_array2)
+calcNoteTime
 "Sort"
-Trksort
+Portsort
 "Write to file"
-ToFile($time_array, $time_array2)
-return "Done, check files $output and $output2"
+MathAndWrite $time_array
+return "Done, check file: $output"
